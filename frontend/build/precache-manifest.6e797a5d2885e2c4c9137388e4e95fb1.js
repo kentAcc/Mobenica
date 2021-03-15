a28360009ed4d6a3f68dda2257ff19ba9e930e64
CASTILLO Abraham <Abraham.CASTILLO@edenred.com>
2021-03-15 10:56:10 -0600

    2  commit

---
+++ frontend/build/precache-manifest.6e797a5d2885e2c4c9137388e4e95fb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cdc11d8d478021da18e273040e949bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a1b8fc0fc22fed4fbb4",
+    "url": "/static/css/main.d7d5f868.chunk.css"
+  },
+  {
+    "revision": "eae47cee81277aaa7df6",
+    "url": "/static/js/2.347e8b6e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.347e8b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a1b8fc0fc22fed4fbb4",
+    "url": "/static/js/main.07c00ba3.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
