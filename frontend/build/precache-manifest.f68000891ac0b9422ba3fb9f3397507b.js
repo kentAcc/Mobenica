537f1efdd8f5a4aef805b8663cc1e5bff0a4799d
Kent _ <abraham.kent.7@gmail.com>
2021-06-22 02:25:11 -0500

    2 commit

---
+++ frontend/build/precache-manifest.f68000891ac0b9422ba3fb9f3397507b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028b0f0c0953c13107f142f8550432d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab4602ab26e6b386e8b5",
+    "url": "/static/css/main.5e7c230b.chunk.css"
+  },
+  {
+    "revision": "d3818c8c1da1df22c856",
+    "url": "/static/js/2.5dc686b5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5dc686b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab4602ab26e6b386e8b5",
+    "url": "/static/js/main.9d5b4ea8.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
